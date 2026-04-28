ac916aa67ca4d2fc6905e2d86563cb574ac3164e
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-04-27 18:19:03 -0700

    Updated main.hpp

--- main.hpp
+++ main.hpp
@@ -6,4 +6,14 @@ int hanoi(int n, char from, char to, char aux);
 int hanoi(int n, char from, char to, char aux)
 {
     // TODO
+    static int count = 0;
+    count ++;
+    if (n == 1){
+        cout << "Move disk " << n << " from " << from << " to " << to << endl;
+        return 0;
+    }
+    hanoi(n - 1, from, aux, to);
+    cout <<"Move disk: " << n << " from " << from << " to " << to << endl;
+    hanoi(n - 1, aux, to, from);
+    return count;
 }
