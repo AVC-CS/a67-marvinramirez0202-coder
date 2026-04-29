66d459dd3dab2415a9bf700f5d96b4e8dc3bbff9
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-04-29 12:54:51 -0700

    Updated main.hpp

--- main.hpp
+++ main.hpp
@@ -10,7 +10,7 @@ int hanoi(int n, char from, char to, char aux)
     count ++;
     if (n == 1){
         cout << "Move disk " << n << " from " << from << " to " << to << endl;
-        return 0;
+        return 1;
     }
     hanoi(n - 1, from, aux, to);
     cout <<"Move disk: " << n << " from " << from << " to " << to << endl;
